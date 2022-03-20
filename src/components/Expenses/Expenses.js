59bc047dc97e185a5f376f12e802f27495594b50
subender <subender.io@outlook.com>
2022-03-20 06:22:09 +0530

    practeced child to parent comm.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+
 function Expenses(props) {
   const [filterYear, setFilterYear] = useState("2019");
   const filterChangeHandler = (selectedYear) => {
@@ -13,18 +14,6 @@ function Expenses(props) {
     (item) => item.date.getFullYear().toString() === filterYear
   );
 
-  let expensesContent = <p>No expenses found</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((item) => (
-      <ExpenseItem
-        key={item.id}
-        title={item.title}
-        amount={item.amount}
-        date={item.date}
-      ></ExpenseItem>
-    ));
-  }
   return (
     <div>
       <Card className="expenses">
@@ -32,7 +21,7 @@ function Expenses(props) {
           selected={filterYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
